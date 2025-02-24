4a6862181fab7be6c0c5a32904b17fcc0d7ab6b8
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2025-02-23 20:31:08 -0800

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -118,8 +118,6 @@ def reset_update_tools():
     if os.name == 'nt': subprocess.run(["cmd", "/c", "rmdir", "/s", "/q", ".git"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     else: subprocess.run(["rm", "-rf", ".git"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print(f"{GREEN_FONT}Update complete. All files have been replaced.{RESET_FONT}")
-    input(f"{GREEN_FONT}Press Enter to continue...{RESET_FONT}")
-    subprocess.run([sys.executable, os.path.join(os.getcwd(), "Menu.py")])
 def about_tools():
     display_logo()
     print("PalWorldSaveTools, all in one tool for fixing/transferring/editing/etc PalWorld saves.")
